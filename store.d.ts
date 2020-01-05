7297c5d02f9e3d489a83fc36f8ed6ae43e48e3f4
Brian Takita <brian.takita@gmail.com>
2020-01-04 19:02:15 -0500

    feat: store: b__ factories

--- store.d.ts
+++ store.d.ts
@@ -1,4 +1,8 @@
+export declare const b__auth0Lock: (ctx?: any, opts?: any) => import("svelte/store").Writable<any>;
 export declare const __auth0Lock: import("svelte/store").Writable<any>;
+export declare const b__logout__Auth0Lock: (ctx?: any, opts?: any) => import("svelte/store").Writable<any>;
 export declare const __logout__Auth0Lock: import("svelte/store").Writable<any>;
+export declare const b__AUTH0_CLIENT_ID: (ctx?: any, opts?: any) => import("svelte/store").Writable<any>;
 export declare const __AUTH0_CLIENT_ID: import("svelte/store").Writable<any>;
+export declare const b__AUTH0_DOMAIN: (ctx?: any, opts?: any) => import("svelte/store").Writable<any>;
 export declare const __AUTH0_DOMAIN: import("svelte/store").Writable<any>;
--- store.ts
+++ store.ts
@@ -1,5 +1,14 @@
 import { writable } from 'svelte/store'
-export const __auth0Lock = writable(null)
-export const __logout__Auth0Lock = writable(null)
-export const __AUTH0_CLIENT_ID = writable(null)
-export const __AUTH0_DOMAIN = writable(null)
+import { _b } from '@ctx-core/object'
+export const b__auth0Lock = _b('__auth0Lock', ()=>
+	writable(null))
+export const __auth0Lock = b__auth0Lock()
+export const b__logout__Auth0Lock = _b('__logout__Auth0Lock', ()=>
+	writable(null))
+export const __logout__Auth0Lock = b__logout__Auth0Lock()
+export const b__AUTH0_CLIENT_ID = _b('__AUTH0_CLIENT_ID', ()=>
+	writable(null))
+export const __AUTH0_CLIENT_ID = b__AUTH0_CLIENT_ID()
+export const b__AUTH0_DOMAIN = _b('__AUTH0_DOMAIN', ()=>
+	writable(null))
+export const __AUTH0_DOMAIN = b__AUTH0_DOMAIN()
