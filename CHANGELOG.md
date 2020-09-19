982b772d1962c04b6a9ad89bf2c766d3794a27bb
Brian Takita <brian.takita@gmail.com>
2020-09-19 15:17:01 -0700

    typescript strict checking

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,22 @@
 # @ctx-core/auth0-lock
 
+## 8.0.0
+
+### Major Changes
+
+- Typescript strict checking
+
+### Patch Changes
+
+- Updated dependencies [undefined]
+  - @ctx-core/auth0@21.0.0
+  - @ctx-core/env@9.0.0
+  - @ctx-core/error@7.0.0
+  - @ctx-core/function@16.0.0
+  - @ctx-core/object@12.0.0
+  - @ctx-core/package@5.0.0
+  - @ctx-core/store@17.0.0
+
 ## 7.0.1
 
 ### Patch Changes
--- store.ts
+++ store.ts
@@ -1,10 +1,15 @@
 import { _b } from '@ctx-core/object'
-import { writable } from '@ctx-core/store'
+import { Writable, writable } from '@ctx-core/store'
+import type { maybe_null } from '@ctx-core/function'
 export const b__auth0Lock = _b('__auth0Lock', ()=>
 	writable(null))
 export const __auth0Lock = b__auth0Lock()
-export const b__logout_Auth0Lock = _b('__logout_Auth0Lock', ()=>
-	writable(null))
+export type $type__logout_Auth0Lock = () => Promise<void>
+export type $maybe_type__logout_Auth0Lock = maybe_null<$type__logout_Auth0Lock>
+export type type__logout_Auth0Lock = Writable<$maybe_type__logout_Auth0Lock>
+export const b__logout_Auth0Lock = _b<type__logout_Auth0Lock>('__logout_Auth0Lock', ()=>
+	writable(null) as type__logout_Auth0Lock
+)
 export const __logout_Auth0Lock = b__logout_Auth0Lock()
 export const b__AUTH0_CLIENT_ID = _b('__AUTH0_CLIENT_ID', ()=>
 	writable(null))
