0528a7a136eccce0961e11ec9e90dbbbaf2070be
Brian Takita <brian.takita@gmail.com>
2020-12-16 22:44:14 -0700

    interface instead of type: auth0_lock_type

--- auth0_lock_b.d.ts
+++ auth0_lock_b.d.ts
@@ -1,4 +1,7 @@
 import type Auth0Lock from 'auth0-lock';
-export declare const auth0_lock_b: (ctx: object, opts?: import("@ctx-core/object")._be_opts_type | undefined) => import("@ctx-core/store").Writable<Auth0LockStatic | null>;
+import { Writable } from '@ctx-core/store';
+export declare const auth0_lock_b: (ctx: object, opts?: import("@ctx-core/object")._be_opts_type | undefined) => auth0_lock_type;
 export declare type $auth0_lock_type = typeof Auth0Lock;
+export interface auth0_lock_type extends Writable<$auth0_lock_type | null> {
+}
 export { auth0_lock_b as b__auth0_lock, };
--- auth0_lock_b.ts
+++ auth0_lock_b.ts
@@ -1,9 +1,11 @@
 import type Auth0Lock from 'auth0-lock'
 import { _b } from '@ctx-core/object'
-import { writable } from '@ctx-core/store'
+import { writable, Writable } from '@ctx-core/store'
 export const auth0_lock_b = _b('auth0_lock', ()=>
-	writable<$auth0_lock_type|null>(null))
+	writable<$auth0_lock_type|null>(null) as auth0_lock_type
+)
 export type $auth0_lock_type = typeof Auth0Lock
+export interface auth0_lock_type extends Writable<$auth0_lock_type|null> {}
 export {
 	auth0_lock_b as b__auth0_lock,
 }
