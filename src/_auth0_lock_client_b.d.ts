8302c4fb4fd4c2e3e6e7ee20b618f7388387674a
Brian Takita <brian.takita@gmail.com>
2021-05-24 19:40:40 -0400

    ## 9.3.0
    ### Minor Changes
    
    - dist,types directory: addressing typescript build issues
    
    ### Patch Changes
    
    - Updated dependencies [undefined]
    - @ctx-core/auth0@22.3.0
    - @ctx-core/env@10.3.0
    - @ctx-core/error@8.2.0
    - @ctx-core/function@17.10.0
    - @ctx-core/object@18.1.0
    - @ctx-core/package@6.3.0
    - @ctx-core/store@24.9.0

--- src/_auth0_lock_client_b.d.ts
+++
@@ -1,7 +0,0 @@
-import Auth0Lock from 'auth0-lock';
-import { B } from '@ctx-core/object';
-import type { auth0_lock_Ctx } from './auth0_lock_Ctx';
-declare const key = "_auth0_lock_client";
-export declare const _auth0_lock_client_b: B<auth0_lock_Ctx, typeof key>;
-export declare type _auth0_lock_client_T = (options: Auth0LockConstructorOptions) => typeof Auth0Lock;
-export {};
--- src/auth0_lock_Ctx.d.ts
+++
@@ -1,9 +0,0 @@
-import type { _auth0_lock_client_T } from './_auth0_lock_client_b';
-import type { auth0_lock_T } from './auth0_lock_b';
-import type { logout_auth0_lock_T } from './logout_auth0_lock_b';
-import type { auth0_Ctx } from '@ctx-core/auth0/src/auth0_Ctx';
-export interface auth0_lock_Ctx extends auth0_Ctx {
-    _auth0_lock_client?: _auth0_lock_client_T;
-    auth0_lock?: auth0_lock_T;
-    logout_auth0_lock?: logout_auth0_lock_T;
-}
