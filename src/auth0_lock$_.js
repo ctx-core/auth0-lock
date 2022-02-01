383c6d405f235db6fdd7845b6113d78e094d347b
Brian Takita <brian.takita@gmail.com>
2022-01-31 18:29:14 -0600

    inlined const key

--- src/auth0_lock$_.js
+++ src/auth0_lock$_.js
@@ -1,5 +1,4 @@
 import { atom$ } from '@ctx-core/nanostores'
 import { be_ } from '@ctx-core/object'
-const key = 'auth0_lock$'
 /** @type {import('./auth0_lock$_.d.ts').auth0_lock$_} */
-export const auth0_lock$_ = be_(key, ()=>atom$(undefined))
+export const auth0_lock$_ = be_('auth0_lock$', ()=>atom$(undefined))
