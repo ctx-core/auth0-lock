eb34f28ccb8a32a47ba1fc3f293e52259e4bb7be
Brian Takita <brian.takita@gmail.com>
2022-05-30 01:40:28 -0500

    update dependencies

--- src/auth0_lock$_.js
+++ src/auth0_lock$_.js
@@ -1,5 +1,5 @@
-import { atom$ } from '@ctx-core/nanostores'
+import { atom_ } from '@ctx-core/nanostores'
 import { be_ } from '@ctx-core/object'
 /** @type {import('./auth0_lock$_.d.ts').auth0_lock$_} */
 export const auth0_lock$_ = be_('auth0_lock$', ()=>
-	atom$(undefined))
+	atom_(undefined))
