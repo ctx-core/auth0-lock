b1b97bbbee8142bb2d3916bacc55483f010dec28
Brian Takita <brian.takita@gmail.com>
2023-02-28 14:22:32 -0500

    formatting

--- src/AUTH0_LOCK_URL/index.js
+++ src/AUTH0_LOCK_URL/index.js
@@ -3,4 +3,6 @@ import { AUTH0_LOCK_URL_ } from '../AUTH0_LOCK_URL_/index.js'
 if (!import_meta_env_().AUTH0_CLIENT_ID) missing_env__throw('AUTH0_CLIENT_ID')
 if (!import_meta_env_().AUTH0_DOMAIN) missing_env__throw('AUTH0_DOMAIN')
 /** @type {typeof import('./index.d.ts').AUTH0_LOCK_URL} */
-export const AUTH0_LOCK_URL = import_meta_env_().AUTH0_LOCK_URL || AUTH0_LOCK_URL_()
+export const AUTH0_LOCK_URL =
+	import_meta_env_().AUTH0_LOCK_URL
+	|| AUTH0_LOCK_URL_()
