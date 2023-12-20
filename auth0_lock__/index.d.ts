cb00cc7b5b81f7e32a5988c2a1a6621ea8b386c8
Brian Takita <brian.takita@gmail.com>
2023-12-20 04:33:21 -0500

    consolidate dependencies

--- auth0_lock__/index.d.ts
+++ auth0_lock__/index.d.ts
@@ -1,10 +1,10 @@
-import type { be_atom_triple_T } from '@ctx-core/nanostores'
+import type { be_sig_triple_T } from 'ctx-core/rmemo'
 import type Auth0Lock from 'auth0-lock'
 export declare const [
 	auth0_lock__,
 	auth0_lock_,
 	auth0_lock__set,
-]: be_atom_triple_T<auth0_lock_T>
+]: be_sig_triple_T<auth0_lock_T>
 export declare type auth0_lock_T = typeof Auth0Lock
 export {
 	auth0_lock__ as auth0_lock$_,
--- auth0_lock__/index.js
+++ auth0_lock__/index.js
@@ -1,11 +1,11 @@
+/// <reference types="ctx-core" />
 /// <reference types="./index.d.ts" />
-import { atom_, be_atom_triple_ } from '@ctx-core/nanostores'
-/** @typedef {import('@ctx-core/nanostores').be_atom_triple_T} */
+import { sig_, be_sig_triple_ } from 'ctx-core/rmemo'
 export const [
 	auth0_lock__,
 	auth0_lock_,
 	auth0_lock__set,
-] = /** @type {be_atom_triple_T<auth0_lock_T>} */ be_atom_triple_(()=>
-	atom_(undefined)
+] = /** @type {be_sig_triple_T<auth0_lock_T>} */ be_sig_triple_(()=>
+	sig_(undefined)
 )
 export { auth0_lock__ as auth0_lock$_ }
